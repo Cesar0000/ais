92d878741afa35b3ec919adcc335766f5e7579d3
Roberto Cesar <rc20022100@gmail.com>
2024-09-16 02:38:38 -0300

    Make adjustments for deployment

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,13 +1,15 @@
+import dj_database_url
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-s4^87g@s48u7&3j-tt8yr8c4l+p3x7u7mldv+=c3ob@cr*ty*$'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower == "true"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
@@ -64,6 +66,8 @@ DATABASES = {
     }
 }
 
+database_url = os.environ.get("DATABASE_URL")
+DATABASES["default"] = dj_database_url.parse(database_url)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
